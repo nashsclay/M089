ef8c3dc5f76fc75e87dc82c62ee586e2e3281b30
Matze089 <74567858+Matze089@users.noreply.github.com>
2020-11-23 19:31:47 +0100

    Update multikfx.sh

--- multikfx.sh
+++ multikfx.sh
@@ -1,40 +1,54 @@
-#!/bin/bash
-
-PORT=15858
-RPCPORT=15959
-CONF_DIR=~/.delion
-SNAPSHOT_DIR=~/snapshot
+#!/bin/sh
 
 cd ~
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
+
+
+
 if [[ $(lsb_release -d) = *16.04* ]]; then
-  COINZIP='https://github.com/delioncoin/delioncore/releases/download/1.2.1/ubuntu16.zip'
+  COINZIP='https://github.com/KnoxFS/kfx-wallet/releases/download/3.2.0/kfx-3.2.0-x86_64-Linux.tar.gz'
 fi
+
+if [[ $(lsb_release -d) = *20.04* ]]; then
+  COINZIP='https://github.com/KnoxFS/kfx-wallet/releases/download/3.2.0/kfx-3.2.0-x86_64-Linux.tar.gz'
+fi
+
+
 if [[ $(lsb_release -d) = *18.04* ]]; then
-  COINZIP='https://github.com/delioncoin/delioncore/releases/download/1.2.1/ubuntu18.zip'
+  COINZIP='https://github.com/KnoxFS/kfx-wallet/releases/download/3.2.0/kfx-3.2.0-x86_64-Linux.tar.gz'
 fi
 if [[ $EUID -ne 0 ]]; then
    echo -e "${RED}$0 must be run as root.${NC}"
    exit 1
 fi
 
+kfx
+
+if [[ $(lsb_release -d) != *20.04* ]]; then
+  echo -e "${RED}You are not running Ubuntu 20.04. Installation could be cancelled.${NC}"
+  exit 1
+fi
+if [[ $EUID -ne 0 ]]; then
+   echo -e "${RED}$0 must be run as root.${NC}"
+   exit 1
+fi
 function configure_systemd {
-  cat << EOF > /etc/systemd/system/delion.service
+  cat -- EOF - /etc/systemd/system/kfxd$ALIAS.service
 [Unit]
-Description=Delion Core Service
+Description=kfxd$ALIAS service
 After=network.target
 [Service]
 User=root
 Group=root
 Type=forking
-#PIDFile=/root/.delion/deliond.pid
-ExecStart=/usr/local/bin/deliond
-ExecStop=-/usr/local/bin/delion-cli stop
+#PIDFile=/root/.kfx_$ALIAS/kfxd.pid
+ExecStart=/root/kfxbin/kfxd_$ALIAS.sh
+ExecStop=-/root/kfxbin/kfx-cli_$ALIAS.sh stop
 Restart=always
 PrivateTmp=true
-TimeoutStopSec=60s
+TimeoutStopSec=30s
 TimeoutStartSec=10s
 StartLimitInterval=120s
 StartLimitBurst=5
@@ -43,60 +57,144 @@ WantedBy=multi-user.target
 EOF
   systemctl daemon-reload
   sleep 6
-  crontab -l > crondelion
-  echo "@reboot systemctl start delion" >> crondelion
-  crontab crondelion
-  rm crondelion
-  systemctl start delion.service
+  crontab -l -- cron$ALIAS
+  echo "@reboot systemctl start kfxd$ALIAS" -- cron$ALIAS
+  crontab cron$ALIAS
+  rm cron$ALIAS
+  systemctl start kfxd$ALIAS.service
 }
 
+clear
+echo "1 - Create new nodes"
+echo "2 - Remove an existing node"
+echo "3 - Upgrade an existing node"
+echo "4 - List aliases"
+echo "What would you like to do?"
+read DO
 echo ""
-echo ""
-DOSETUP="y"
-
-if [ $DOSETUP = "y" ]  
+if [ $DO = "4" ]
 then
-  sudo apt-get update
+ALIASES=$(find /root/.delion_* -maxdepth 0 -type d | cut -c22-)
+echo -e "${GREEN}${ALIASES}${NC}"
+echo ""
+echo "1 - Create new nodes"
+echo "2 - Remove an existing node"
+echo "3 - Upgrade an existing node"
+echo "4 - List aliases"
+echo "What would you like to do?"
+read DO
+echo ""
+fi
+if [ $DO = "3" ]
+then
+perl -i -ne 'print if ! $a{$_}++' /etc/monit/monitrc >/dev/null 2>&1
+echo "Enter the alias of the node you want to upgrade"
+read ALIAS
+  echo -e "Upgrading ${GREEN}${ALIAS}${NC}. Please wait."
+  sed -i '/$ALIAS/d' .bashrc
+  sleep 1
+  ## Config Alias
+  echo "alias ${ALIAS}_status=\"kfx-cli -datadir=/root/.kfx_$ALIAS masternode status\"" -- .bashrc
+  echo "alias ${ALIAS}_stop=\"kfx-cli -datadir=/root/.kfx_$ALIAS stop && systemctl stop kfxd$ALIAS\"" -- .bashrc
+  echo "alias ${ALIAS}_start=\"/root/kfxbin/kfx_${ALIAS}.sh && systemctl start kfxd$ALIAS\""  -- .bashrc
+  echo "alias ${ALIAS}_config=\"nano /root/.kfx_${ALIAS}/kfx.conf\""  -- .bashrc
+  echo "alias ${ALIAS}_getinfo=\"kfx-cli -datadir=/root/.kfx_$ALIAS getinfo\"" -- .bashrc
+  configure_systemd
+  sleep 1
+  source .bashrc
+  echo -e "${GREEN}${ALIAS}${NC} Successfully upgraded."
+fi
+if [ $DO = "2" ]
+then
+perl -i -ne 'print if ! $a{$_}++' /etc/monit/monitrc --/dev/null 2>&1
+echo "Input the alias of the node that you want to delete"
+read ALIASD
+echo ""
+echo -e "${GREEN}Deleting ${ALIASD}${NC}. Please wait."
+## Removing service
+systemctl stop kfxd$ALIASD >/dev/null 2--&1
+systemctl disable kfxd$ALIASD >/dev/null 2--&1
+rm /etc/systemd/system/kfxd${ALIASD}.service --/dev/null 2>&1
+systemctl daemon-reload >/dev/null 2--&1
+systemctl reset-failed >/dev/null 2--&1
+## Stopping node
+kfx-cli -datadir=/root/.kfx_$ALIASD stop --/dev/null 2>&1
+sleep 5
+## Removing monit and directory
+rm /root/.kfx_$ALIASD -r --/dev/null 2>&1
+sed -i '/$ALIASD/d' .bashrc --/dev/null 2>&1
+sleep 1
+sed -i '/$ALIASD/d' /etc/monit/monitrc >/dev/null 2--&1
+monit reload >/dev/null 2>&1
+sed -i '/$ALIASD/d' /etc/monit/monitrc >/dev/null 2>&1
+crontab -l -u root | grep -v kfxd$ALIASD | crontab -u root - >/dev/null 2>&1
+source .bashrc
+echo -e "${ALIASD} Successfully deleted."
+fi
+if [ $DO = "1" ]
+then
+echo "1 - Easy mode"
+echo "2 - Expert mode"
+echo "Please select a option:"
+read EE
+echo ""
+if [ $EE = "1" ] 
+then
+MAXC="16"
+fi
+if [ $EE = "2" ] 
+then
+echo ""
+echo "Enter max connections value"
+read MAXC
+fi
+DOSETUP="y"
+if [ $DOSETUP = "y" ]
+then
+  echo -e "Installing ${BLUE} KFX coin dependencies${NC}. Please wait."
+  sudo apt-get update 
   sudo apt-get -y upgrade
   sudo apt-get -y dist-upgrade
   sudo apt-get update
-  sudo apt-get install build-essential zip unzip libtool autotools-dev autoconf pkg-config libssl-dev libboost-all-dev libminiupnpc-dev software-properties-common -y && add-apt-repository ppa:bitcoin/bitcoin && apt-get update -y && apt-get install libdb4.8-dev libdb4.8++-dev libminiupnpc-dev libzmq3-dev bc curl nano libevent-pthreads-2.0-5 -y
-
-  cd /var
-  sudo touch swap.img
-  sudo chmod 600 swap.img
-  sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
-  sudo mkswap /var/swap.img
-  sudo swapon /var/swap.img
-  sudo free
-  sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
+  sudo apt-get install build-essential libtool autotools-dev autoconf pkg-config libssl-dev libboost-all-dev libminiupnpc-dev software-properties-common -y && add-apt-repository ppa:bitcoin/bitcoin && apt-get update -y && apt-get install libdb4.8-dev libdb4.8++-dev libminiupnpc-dev libzmq3-dev libevent-pthreads-2.0-5 zip unzip bc curl nano -y
+  sleep 2
   cd
-  
-  wget $COINZIP
-  unzip *.zip
-  chmod +x delion*
-  rm delion-qt delion-tx *.zip
-  sudo cp delion* /usr/local/bin
-  mkdir -p delion
-  sudo mv delion-cli deliond /root/delion
-  
-  mkdir -p $SNAPSHOT_DIR
-  cd $SNAPSHOT_DIR
-  wget http://cdn.delion.xyz/snapshot.zip
-  unzip snapshot.zip
-  rm snapshot.zip
-
+  chmod +x /root/kfx/*
+  sudo cp -R  /root/kfx/* /usr/local/bin
+  mkdir -p ~/kfxbin 
+  echo 'export PATH=~/kfxbin:$PATH' > ~/.bash_aliases
+  source ~/.bashrc
+  echo ""
 fi
 
- IP=$(curl -s4 api.ipify.org)
+echo "How many nodes do you want to install on this server?"
+read MNCOUNT
+echo "Enter Starting RPC PORT"
+read RPCPORT
+let COUNTER=0
+while [  $COUNTER -lt $MNCOUNT ]; do
  echo ""
- echo "Configure your masternodes now!"
- echo "Detecting IP address:$IP"
+ echo "Enter IP ADDRESS"
+ read IP6
  echo ""
- echo "Enter masternode private key"
- read PRIVKEY
- 
- mkdir -p $CONF_DIR
+ PORT=19328
+ PORTD=19328
+ RPCPORT=$(($RPCPORT+$COUNTER))
+  echo ""
+  echo "Enter alias for new node"
+  read ALIAS
+  CONF_DIR=~/.delion_$ALIAS
+  echo ""
+  echo "Enter masternode private key for node $ALIAS"
+  read PRIVKEY
+  mkdir ~/.kfx_$ALIAS
+  cp -R /root/delionsnapshot/* ~/.delion_$ALIAS
+  echo '#!/bin/bash' > ~/delionbin/deliond_$ALIAS.sh
+  echo "deliond -daemon -conf=$CONF_DIR/delion.conf -datadir=$CONF_DIR "'$*' >> ~/delionbin/deliond_$ALIAS.sh
+  echo '#!/bin/bash' > ~/delionbin/delion-cli_$ALIAS.sh
+  echo "delion-cli -conf=$CONF_DIR/delion.conf -datadir=$CONF_DIR "'$*' >> ~/delionbin/delion-cli_$ALIAS.sh
+  chmod 755 ~/delionbin/delion*.sh
+  mkdir -p $CONF_DIR
   echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> delion.conf_TEMP
   echo "rpcpassword=pass"`shuf -i 100000-10000000 -n 1` >> delion.conf_TEMP
   echo "rpcallowip=127.0.0.1" >> delion.conf_TEMP
@@ -105,34 +203,49 @@ fi
   echo "server=1" >> delion.conf_TEMP
   echo "daemon=1" >> delion.conf_TEMP
   echo "logtimestamps=1" >> delion.conf_TEMP
-  echo "maxconnections=250" >> delion.conf_TEMP
+  echo "maxconnections=$MAXC" >> delion.conf_TEMP
   echo "masternode=1" >> delion.conf_TEMP
   echo "dbcache=20" >> delion.conf_TEMP
   echo "maxorphantx=5" >> delion.conf_TEMP
   echo "maxmempool=100" >> delion.conf_TEMP
   echo "" >> delion.conf_TEMP
-  echo "port=$PORT" >> delion.conf_TEMP
-  echo "externalip=$IP:$PORT" >> delion.conf_TEMP
-  echo "masternodeaddr=$IP:$PORT" >> delion.conf_TEMP
+  echo "" >> delion.conf_TEMP
+  echo "bind=$IP6" >> delion.conf_TEMP
+  echo "port=$PORTD" >> delion.conf_TEMP
+  echo "externalip=$IP6:$PORT" >> delion.conf_TEMP
+  echo "masternodeaddr=$IP6:$PORT" >> delion.conf_TEMP
   echo "masternodeprivkey=$PRIVKEY" >> delion.conf_TEMP
+  echo "" >> delion.conf_TEMP
+  echo "addnode=45.77.252.62:15858" >> delion.conf_TEMP
+  echo "addnode=45.77.226.43:15858" >> delion.conf_TEMP
+  echo "addnode=45.76.150.140:15858" >> delion.conf_TEMP
+  echo "addnode=45.63.92.158:15858" >> delion.conf_TEMP
   mv delion.conf_TEMP $CONF_DIR/delion.conf
   echo ""
-  echo -e "Your ip is ${GREEN}$IP:$PORT${NC}"
-
-  ## copy snapshot
-  cp -R $SNAPSHOT_DIR/* $CONF_DIR
+  echo -e "Your ip is ${GREEN}$IP6:$PORT${NC}"
+  COUNTER=$((COUNTER+1))
+	echo "alias ${ALIAS}_status=\"kfx-cli -datadir=/root/.delion_$ALIAS masternode status\"" >> .bashrc
+	echo "alias ${ALIAS}_stop=\"systemctl stop kfxd$ALIAS\"" >> .bashrc
+	echo "alias ${ALIAS}_start=\"systemctl start kfxd$ALIAS\""  >> .bashrc
+	echo "alias ${ALIAS}_config=\"nano /root/.kfx_${ALIAS}/delion.conf\""  >> .bashrc
+	echo "alias ${ALIAS}_getinfo=\"delion-cli -datadir=/root/.kfx_$ALIAS getinfo\"" >> .bashrc
+	echo "alias ${ALIAS}_resync=\"/root/kfxbin/kfxd_$ALIAS -resync\"" >> .bashrc
+	echo "alias ${ALIAS}_reindex=\"/root/kfxbin/deliond_$ALIAS -reindex\"" >> .bashrc
 	## Config Systemctl
 	configure_systemd
-  
+done
 echo ""
 echo "Commands:"
-echo -e "Start Delion Service: ${GREEN}systemctl start delion${NC}"
-echo -e "Check Delion Status Service: ${GREEN}systemctl status delion${NC}"
-echo -e "Stop Delion Service: ${GREEN}systemctl stop delion${NC}"
-echo -e "Check Masternode Status: ${GREEN}delion-cli masternode status${NC}"
-
+echo "ALIAS_start"
+echo "ALIAS_status"
+echo "ALIAS_stop"
+echo "ALIAS_config"
+echo "ALIAS_getinfo"
+echo "ALIAS_resync"
+echo "ALIAS_reindex"
+fi
 echo ""
-echo -e "${GREEN}Delion Masternode Installation Done${NC}"
+echo "provided by Matze089 for the strong KFX Community"
 exec bash
 exit
 
